e13a3a3d59d33da9496f65d2aeb4df831b31f8a7
sashitaghimire <ghimiresashita@gmail.com>
2020-09-29 13:02:09 +0545

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Daraz clone</h1>
+      <Header />
+
     </div>
   );
 }
